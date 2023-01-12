38a3060e9fc7478f45fb511809d2679a6d8456eb
rinkuberde <rinkuberde@gmail.com>
2023-01-12 20:28:53 +0530

    [Rinku] - new contact added

--- Address-Book-System/Program.cs
+++ Address-Book-System/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {   //print Message
-            Console.WriteLine("Welcome to AddressBook Book System");
+            Console.WriteLine("Welcome to Address Book  System");
 
-            //add a new contact
-            CreateContact.PersonDetails();
-            Console.ReadLine();
+              //Create contact
+              CreateContact.PersonDetails();
+              Console.ReadLine();
+
+             //add a new contact
+              AddContact.GetCustomer();
+              AddContact.ListingPeople();
         }
     }
 }
