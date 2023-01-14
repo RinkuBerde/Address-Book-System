59247c3d488eb3eea511545e0151afec8e614278
rinkuberde <rinkuberde@gmail.com>
2023-01-14 18:26:31 +0530

    [Rinku] -add multiple person to Address Book

--- Address-Book-System/Program.cs
+++ Address-Book-System/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Address_Book_System
+﻿using System.Net;
+
+namespace Address_Book_System
 {
     internal class Program
     {
@@ -25,6 +27,38 @@
             DeleteContact.ListingPeople();
             DeleteContact.RemovePeople();
 
+            //add multiple person to Address Book
+            Console.WriteLine("Welcome To Address Book System!");
+            Console.WriteLine("1.Enter to add the details");
+            Console.WriteLine("2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove the details");
+            Console.WriteLine("Enter a option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddressBook.AddBook.GetCustomer();
+                    AddressBook.AddBook.ListingPeople();
+                    break;
+                case "2":
+                    AddressBook.AddBook.GetCustomer();
+                    AddressBook.AddBook.Modify();
+                    AddressBook.AddBook.ListingPeople();
+                    break;
+                case "3":
+                    AddressBook.AddBook.GetCustomer();
+                    AddressBook.AddBook.ListingPeople();
+                    break;
+                case "4":
+                    AddressBook.AddBook.GetCustomer();
+                    AddressBook.AddBook.RemovePeople();
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+            }
         }
     }
 }
