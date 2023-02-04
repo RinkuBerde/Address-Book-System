04967ba4c844a84db8911a1772b7cf53cfd4c57f
rinkuberde <rinkuberde@gmail.com>
2023-02-04 23:02:14 +0530

    [Rinku] - Added code  to sort entries by state and zip

--- Address-Book-System/CreateContact.cs
+++ Address-Book-System/CreateContact.cs
@@ -6,46 +6,16 @@ using System.Threading.Tasks;
 
 namespace Address_Book_System
 {
-    internal class CreateContact
+    interface IContacts
     {
-        public static void PersonDetails()
-        {
-            Console.Write("Enter First Name: ");
-            String firstName = Console.ReadLine();
-
-            Console.Write("Enter Last Name: ");
-            string lastName = Console.ReadLine();
-
-            Console.Write("Enter Address : ");
-            var Addresses = Console.ReadLine();
-
-            Console.Write("Enter City : ");
-            string city = Console.ReadLine();
-
-            Console.Write("Enter State : ");
-            string state = Console.ReadLine();
-
-            Console.Write("Enter ZipCode: ");
-            string zipCode = Console.ReadLine();
-
-            Console.Write("Enter Phone Number: ");
-            string phoneNumber = Console.ReadLine();
-
-            Console.Write("Enter EmailId: ");
-            string Email = Console.ReadLine();
-            //Adding fetched details to list
-            List<string> PersonList = new List<string>();
-            PersonList.Add(firstName);
-            PersonList.Add(lastName);
-            PersonList.Add(city);
-            PersonList.Add(state);
-            PersonList.Add(zipCode);
-            PersonList.Add(Email);
-            foreach (string i in PersonList)
-             Console.WriteLine(i);
-
-        }
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber, string bookName);
+        public void ViewContact(string name, string bookName);
+        public void EditContact(string name, string bookName);
+        public void DeleteContact(string name, string bookName);
+        public void AddAddressBook(string bookName);
     }
 }
+    
+
     
 
